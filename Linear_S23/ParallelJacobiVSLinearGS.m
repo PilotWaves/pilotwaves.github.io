ca60106902cc87d84c6c354595fea66bfbca55e5
Amin <arahman2@uw.edu>
2024-10-25 23:38:54 -0700

    Add files via upload

---
+++ Linear_S23/ParallelJacobiVSLinearGS.m
@@ -0,0 +1,34 @@
+ParallelTime = 0*[0:4];
+LinearTime = ParallelTime;
+
+for n = 0:length(ParallelTime)-1
+
+A = diag([1:10^n]) + .01*randn(10^n);
+b = ones(length(A),1);
+
+y= zeros(length(b),1);
+
+E = 1e-8;
+T = 0;
+
+tic
+while max(abs(A*y - b)) > E
+    y = ParallelJacobi(y,A,b);
+end
+ParallelTime(n+1) = toc;
+
+
+y= zeros(length(b),1);
+
+tic
+while max(abs(A*y - b)) > E
+    y = LinearGaussSeidel(y,A,b);
+end
+LinearTime(n+1) = toc;
+
+end
+
+set(0,'defaultAxesFontSize',20)
+semilogy([0:length(ParallelTime)-1],ParallelTime,[0:length(ParallelTime)-1],LinearTime,'Linewidth',5)
+xlabel('n (powers of 10)')
+ylabel('Runtime (log scale)')
